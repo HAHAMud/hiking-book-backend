fc442114895d69b2ebdce1062db7aefaeb71e3d3
chicken <hibary.taiwan@gmail.com>
2023-06-24 03:39:12 +0800

    feat: 註冊進IOC

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { TodoModule } from './features/todo/todo.module';
-// import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
-// import { DatabaseService } from 'database.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './features/todo/todo.module';
+import { CopyTodoModule } from './features/copy-todo/copy-todo.module';
+import { DatabaseModule } from './database/database.neon';
 
-// @Module({
-//   imports: [TodoModule, CopyTodoModule],
-//   controllers: [AppController],
-//   providers: [AppService, DatabaseService],
-// })
-export class AppModule {
-  // constructor(private readonly databaseService: DatabaseService) {}
-
-  async onModuleInit(): Promise<void> {
-    // await this.databaseService.connect();
-  }
-}
+@Module({
+  imports: [TodoModule, CopyTodoModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Pool } from 'pg';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('DATABASE_POOL') private readonly pool: Pool) { }
+
   getHello(): string {
     return 'Hello World!';
+  }
+  async getPostgresVersion(): Promise<void> {
+    let client;
+    try {
+      client = await this.pool.connect();
+      const res = await client.query('SELECT version()');
+      console.log(res.rows[0]);
+    } catch (error) {
+      console.error('Error connecting to PostgreSQL:', error);
+    } finally {
+      client.release();
+    }
   }
 }
