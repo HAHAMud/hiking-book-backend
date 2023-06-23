6ee1e2db64b068b78857d81614f409ba01514383
Elvina <qq861924@gmail.com>
2023-06-23 22:48:27 +0800

    style: 💄 format

--- src/database/database.neon.ts
+++ src/database/database.neon.ts
@@ -4,19 +4,18 @@ require('dotenv').config();
 const { DATABASE_URL } = process.env;
 
 const pool = new Pool({
-    connectionString: DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+  connectionString: DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function getPostgresVersion() {
-    const client = await pool.connect();
-    try {
-        const res = await client.query('SELECT version()');
-        console.log(res.rows[0]);
-    } finally {
-        client.release();
-    }
+  const client = await pool.connect();
+  try {
+    const res = await client.query('SELECT version()');
+    console.log(res.rows[0]);
+  } finally {
+    client.release();
+  }
 }
-
