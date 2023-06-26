adce9c5961265cf6f0424f313d565152dc008999
Elvina <qq861924@gmail.com>
2023-06-26 14:22:42 +0800

    fix: 🐛 remove useless

--- src/features/copy-todo/copy-todo.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CopyTodoController } from './copy-todo.controller';
-import { TodoModule } from '../todo/todo.module';
-
-@Module({
-  controllers: [CopyTodoController],
-  imports: [TodoModule],
-})
-export class CopyTodoModule {}
