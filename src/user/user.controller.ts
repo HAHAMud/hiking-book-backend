4d476deaa93dc6ef4ceee4b4de8b9927c5317df3
Elvina <qq861924@gmail.com>
2023-06-30 01:13:10 +0800

    feat: 🎸 user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,69 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { UserEntity, UserService } from '.';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  /**
+   * @description 獲取所有的 user 資料
+   * @returns
+   */
+  @Get()
+  async findAll(): Promise<UserEntity[]> {
+    return await this.userService.getUserList();
+  }
+
+  /**
+   * @param id
+   * @description 透過 id 獲取 user 資料
+   * @returns
+   */
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<UserEntity> {
+    return await this.userService.getUserById(id);
+  }
+
+  /**
+   * @param user
+   * @description 新增 user 資料
+   * @returns
+   */
+  @Post()
+  async create(@Body() user: UserEntity): Promise<UserEntity> {
+    return await this.userService.createUser(user);
+  }
+
+  /**
+   *
+   * @param id
+   * @param user
+   * @description 透過 id 更新 user 資料
+   * @returns
+   */
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() user: UserEntity,
+  ): Promise<void> {
+    return await this.userService.updateUser(id, user);
+  }
+
+  /**
+   * @param id
+   * @description 透過 id 刪除 user 資料
+   * @returns
+   */
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<void> {
+    return await this.userService.deleteUser(id);
+  }
+}
