e2c23f6c7e9d117c30d88ec5154cd28058ab4779
Elvina <qq861924@gmail.com>
2023-07-04 00:30:17 +0800

    docs: ✏️ add user migration

---
+++ src/migrations/1688116005155-USER.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class USER1688116005155 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
