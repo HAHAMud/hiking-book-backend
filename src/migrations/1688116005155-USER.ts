4f32727693529883fd649be4202109f25bca5853
chicken <hibary.taiwan@gmail.com>
2023-07-04 11:28:05 +0800

    fix: 補上Migrations設定

--- src/migrations/1688116005155-USER.ts
+++ src/migrations/1688116005155-USER.ts
@@ -1,7 +1,94 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class USER1688116005155 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+export class CreateUserTable1626163918297 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        schema: 'public',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isGenerated: true,
+            generationStrategy: 'increment',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'birthday',
+            type: 'date',
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '20',
+          },
+          {
+            name: 'tex_phone',
+            type: 'varchar',
+            length: '20',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'gender',
+            type: 'smallint',
+          },
+          {
+            name: 'account',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'address',
+            type: 'varchar',
+            length: '200',
+          },
+          {
+            name: 'exmergency_name',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'exmergency_phone',
+            type: 'varchar',
+            length: '20',
+          },
+          {
+            name: 'created_at',
+            type: 'time without time zone',
+            default: 'now()',
+          },
+          {
+            name: 'identity',
+            type: 'varchar',
+            length: '100',
+          },
+          {
+            name: 'updated_at',
+            type: 'time without time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+      true,
+    );
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user');
+  }
 }
